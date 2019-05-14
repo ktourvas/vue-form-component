9743523087fc018158def1256d34bbfbf62d99f1
Kostas Tourvas <k.tourvas@gmail.com>
2019-05-14 16:28:17 +0300

    some commenting

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -8,6 +8,11 @@
 
         props: [ 'action', 'items', 'passthrough' ],
 
+        /**
+         * The form data variable is being initialized with the items and passthrough props being passed to it.
+         * The items prop is a collection of the user visible form elements that will be coupled with the dom.
+         * The passthrough prop is a collection of defined value attributes that will be passed to every request.
+         */
         data() {
             return {
                 success: false,
@@ -27,6 +32,7 @@
              * As well as emitting success and failure events, also sets the success data attribute to true
              * after a successful submission and sets it back to false after 5s so that it can be used for
              * showing relevant messages and similar functionality.
+             *
              * @returns void
              * @emits success
              * @emits failure
